ad8ae727241616df694583979a13a51d88b303bf
Rinchindugar <rinchindugar@tomyo.mn>
2023-12-26 10:51:32 +0800

    feat: 🎸 stop translating

---
+++ stop_translating.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Folder names for crawlers
+CRAWLER_NAMES=(crawler1 crawler2 crawler3 crawler4 crawler5 crawler6)
+CRAWLER_PORTS=(3001 3002 3003 3004 3005 3006)
+PORT_START=3000
+
+# clone repo and create environment for each crawler
+for i in "${!CRAWLER_NAMES[@]}"
+do
+    # pm2 delete
+    pm2 delete translate_${CRAWLER_NAMES[$i]}
+    # kill it
+done
+
+# done with all crawlers
+echo "All crawlers are deleted"
+exit 0
--- translating.sh
+++ translating.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
-# Crawler repo
-CRAWLER_REPO=https://github.com/IT18-Rinchindugar/bundle.git
+# if pm2 is not installed, install it
+if ! [ -x "$(command -v pm2)" ]; then
+    npm install pm2 -g
+fi
 
 # Folder names for crawlers
 CRAWLER_NAMES=(crawler1 crawler2 crawler3 crawler4 crawler5 crawler6)
@@ -17,11 +18,11 @@ do
     if [ -x "$(command -v winpty)" ]; then
         # windows git bash terminal
         # new terminal to run translate script
-        start "Git Bash" "C:\Program Files\Git\git-bash.exe" -i -c "cd $(pwd); ./translate; exec bash"
+        start "Git Bash" "C:\Program Files\Git\git-bash.exe" -i -c "cd $(pwd); pm2 start translate.js --name translate_${CRAWLER_NAMES[$i]} && pm2 logs translate_${CRAWLER_NAMES[$i]}; exec bash"
     else
         # macos terminal
         # new terminal to run script
-        osascript -e "tell app \"Terminal\" to do script \"cd $(pwd) &&  ./translate\""
+        osascript -e "tell app \"Terminal\" to do script \"cd $(pwd) && pm2 start translate.js --name translate_${CRAWLER_NAMES[$i]} && pm2 logs translate_${CRAWLER_NAMES[$i]}\""
 
     fi
     cd ..
